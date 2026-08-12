3def826b3b9a6903dc82d465640bf1045c544949
Picksaw <amirehsan8293@gmail.com>
2026-08-12 03:45:15 +0330

    Initial StormBlade release

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+base: '/StormBlade/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
